d34658531bbfd9559e76685133b6381b504c2572
emmette <emmette@users.noreply.github.com>
2025-07-26 16:45:23 -0500

    fix: Rename useAdminAuth.ts to .tsx and add React import to support JSX syntax

--- frontend/hooks/useAdminAuth.tsx
+++ frontend/hooks/useAdminAuth.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
 import { useAuthStore } from '@/store/authStore';
 
